f9f675bc901e651ea272aa2e4dffc649720faf89
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-01-16 18:22:02 -0800

    Update canPlaceFlowers.py
    added comments

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -1,13 +1,20 @@
 class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
-        count = 0
-        for i in range(len(flowerbed)):
-            if(flowerbed[i]==0):
-                emptyLeft = (i==0 or flowerbed[i-1]==0)
-                emptyRight = (i==len(flowerbed)-1 or flowerbed[i+1]==0)
 
-                if(emptyLeft and emptyRight):
-                    flowerbed[i] = 1
-                    count += 1
-            
-        return count >=n
+        #start count
+        count =0
+
+        #check for 0 then check for left and right 0;s
+        for i in range(len(flowerbed)):
+            if flowerbed[i]==0:
+                leftEmpty = (i==0 or flowerbed[i-1]==0)
+                rightEmpty = (i==len(flowerbed)-1 or flowerbed[i+1]==0)
+            #plant flower and increase count if both left and right are empty
+                if(leftEmpty and rightEmpty):
+                    flowerbed[i]=1
+                    count+=1
+        #check if enough flowers were planted
+        if(count>=n):
+            return True
+        else:
+            return False

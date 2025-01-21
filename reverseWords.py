7af522063544505f3eeebfe8ac4729e5137bd313
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-01-20 19:27:20 -0800

    Create reverseWords.py

---
+++ reverseWords.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseWords(self, s):
+        s=s.split()
+        left,right=0,len(s)-1
+
+        while(left<right):
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+
+        return ' '.join(s)
